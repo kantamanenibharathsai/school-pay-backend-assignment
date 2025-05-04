dc83ab2bb45d103d010c1622b3cac6e64a7bfd0f
kantamanenibharathsai <kantamanenibharath21@gmail.com>
2025-05-05 00:33:09 +0530

    swagger-url-changed

--- swagger/swagger.js
+++ swagger/swagger.js
@@ -11,7 +11,7 @@ const options = {
         },
         servers: [
             { 
-                url: 'http://localhost:5000',
+                url: 'https://school-pay-backend-assignment.onrender.com',
                 description: 'Local development server'
             }
         ],
